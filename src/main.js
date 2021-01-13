5be7bac09f2c7d9b51148a4e242632bdab1a3c80
Roduan Kareem Aldeen <roduan98@gmail.com>
2021-01-13 19:15:29 +0200

    add event bux to open post details dialog

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import router from './router'
 import vuetify from './plugins/vuetify'
 import './plugins/vue-axios'
 import './plugins/vue-moment'
+import EventBus from './plugins/event-bus'
+
+Vue.use(EventBus)
 
 Vue.config.productionTip = false
 
