76eb37bfc1f460b8e86b8c5f61114ccfc369c6c4
AhmadBsh <ahmad.bsh237@gmail.com>
2021-01-15 19:25:59 +0200

    -added some changes on the main card design -added Sign Up tab with

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,77 +1,261 @@
 <template>
-
   <div class="back fill-height">
-    <v-container justify-center fluid fill-height >
-        <v-row justify="center">
-          <v-col cols="3" >
-               <v-card > 
-              <v-tabs  color="deep-purple accent-4" background-color="primary" left>
-                <v-tab class="white--text">SIGN IN</v-tab>
-                <v-tab class="white--text">SIGN UP</v-tab>
-                <v-tab-item>
-        
-          <v-card class="mx-auto elevation-12 midcard " style=" backdrop-filter: blur(5px);
-  background: rgba(255,255,255,.7)  ">
-              <v-card-text>
-                <v-form class="Pdtop">
-                  <v-text-field name="login" label="Login" type="text" ></v-text-field>
-                  <v-text-field name="password" label="Password" id="password" type="password"></v-text-field>
-                </v-form>
-              </v-card-text>
-              <v-card-actions class="Flex"  >
-                <v-spacer ></v-spacer>
-                <v-btn   color="primary"  >Login</v-btn>
-              </v-card-actions>
-            </v-card>
-      </v-tab-item>
+    <v-container justify-center fluid fill-height>
+      <v-row justify="center">
+        <v-col cols="4">
+          <v-card>
+            <v-tabs
+              color="deep-purple accent-4"
+              background-color="primary"
+              left
+            >
+              <v-tab class="white--text">SIGN IN</v-tab>
+              <v-tab class="white--text">SIGN UP</v-tab>
               <v-tab-item>
+                <v-card class="mx-auto elevation-12 midcard Blured">
+                  <v-card-text class="blured">
+                    <v-form
+                      v-model="valid"
+                      lazy-validation
+                      ref="form"
+                      class="py-16 px-7"
+                    >
+                      <v-text-field
+                        name="nameemail"
+                        label="Email / Username"
+                        type="text"
+                        v-model="nameemail"
+                        :rules="nameemailRules"
+                        required
+                      ></v-text-field>
+                      <v-text-field
+                        name="password"
+                        label="Password"
+                        id="password"
+                        type="password"
+                        required
+                        v-model="password"
+                        :rules="passwordRules"
+                      ></v-text-field>
+                    </v-form>
+                  </v-card-text>
+                  <v-card-actions class="flex-column">
+                    <v-spacer></v-spacer>
+                    <v-btn color="primary" @click="validate">Login</v-btn>
+                  </v-card-actions>
+                </v-card>
               </v-tab-item>
-              </v-tabs>
-            </v-card>
-          </v-col>
-        </v-row>
-      </v-container>
+              <v-tab-item>
+                <v-card class="mx-auto elevation-12 midcard Blured">
+                  <v-card-text class="pa-12">
+                    <v-form ref="form" class="py-0 py-2">
+                      <v-text-field
+                        name="username"
+                        label="Username"
+                        type="text"
+                        v-model="name"
+                        :counter="20"
+                        :rules="nameRules"
+                        required
+                      ></v-text-field>
+                      <v-text-field
+                        name="email"
+                        label="Email"
+                        type="email"
+                        required
+                        v-model="email"
+                        :rules="emailRules"
+                      ></v-text-field>
+                      <v-text-field
+                        name="password"
+                        label="Password"
+                        id="password"
+                        type="password"
+                        required
+                        v-model="password"
+                        :rules="passwordRules"
+                      ></v-text-field>
+                      <v-select
+                        :items="specialization"
+                        name="specialization"
+                        label="Specialization"
+                        dense
+                        required v-model="select"
+                        :rules="[(v) => !!v || 'Specialization is required']"
+                      ></v-select>
+                      <v-menu
+                        ref="menu"
+                        v-model="menu"
+                        :close-on-content-click="false"
+                        :return-value.sync="date"
+                        transition="scale-transition"
+                        offset-y
+                        min-width="auto"
+                      >
+                        <template v-slot:activator="{ on, attrs }">
+                          <v-text-field
+                            v-model="date"
+                            label="Choose your birthdate"
+                            prepend-icon="mdi-calendar"
+                            readonly
+                            v-bind="attrs"
+                            v-on="on"
+                          ></v-text-field>
+                        </template>
+                        <v-date-picker v-model="date" no-title scrollable>
+                          <v-spacer></v-spacer>
+                          <v-btn text color="primary" @click="menu = false">
+                            Cancel
+                          </v-btn>
+                          <v-btn
+                            text
+                            color="primary"
+                            @click="$refs.menu.save(date)"
+                          >
+                            OK
+                          </v-btn>
+                        </v-date-picker>
+                      </v-menu>
+                      <v-spacer></v-spacer>
+                      <v-menu
+                        ref="menu2"
+                        v-model="menu2"
+                        :close-on-content-click="false"
+                        :return-value.sync="date2"
+                        transition="scale-transition"
+                        offset-y
+                        min-width="auto"
+                      >
+                        <template v-slot:activator="{ on, attrs }">
+                          <v-text-field
+                            v-model="date2"
+                            label="Choose your registreation year"
+                            prepend-icon="mdi-calendar"
+                            readonly
+                            v-bind="attrs"
+                            v-on="on"
+                          ></v-text-field>
+                        </template>
+                        <v-date-picker v-model="date2" no-title scrollable>
+                          <v-spacer></v-spacer>
+                          <v-btn text color="primary" @click="menu2 = false">
+                            Cancel
+                          </v-btn>
+                          <v-btn
+                            text
+                            color="primary"
+                            @click="$refs.menu2.save(date2)"
+                          >
+                            OK
+                          </v-btn>
+                        </v-date-picker>
+                      </v-menu>
+                      <v-checkbox
+                        v-model="checkbox"
+                        style="margin: 0px; max-height: 50px"
+                        :rules="[(v) => !!v || 'You must agree to continue!']"
+                        label="Do you agree?"
+                        required
+                      >
+                        <template v-slot:label>
+                          <div>I agree to terms and condition</div>
+                        </template>
+                      </v-checkbox>
+                    </v-form>
+                    <v-btn
+                      :disabled="!valid"
+                      color="success"
+                      class="mr-4"
+                      @click="validate"
+                    >
+                      Sign Up
+                    </v-btn>
+
+                    <v-btn color="error" class="mr-4" @click="reset">
+                      Reset Form
+                    </v-btn>
+                  </v-card-text>
+                  <v-card-actions class="flex-column pt-0">
+                    <v-spacer></v-spacer>
+                  </v-card-actions>
+                </v-card>
+              </v-tab-item>
+            </v-tabs>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
-  
 </template>
 <script>
 export default {
-  name: 'Home',
-  components: {
-    // HelloWorld
-  },
+  name: "Home",
+  components: {},
   data: () => ({
+    valid: false,
+    nameemail: "",
+    nameemailRules: [
+      (v) => !!v || "Username/Email is required",
+      (v) => (v && v.length <= 20) || "Name must be less than 10 characters",
+    ],
+    name: "",
+    nameRules: [
+      (v) => !!v || "Name is required",
+      (v) => (v && v.length <= 20) || "Name must be less than 10 characters",
+    ],
+    password: "",
+    passwordRules: [
+      (v) => !!v || "Password is required",
+      (v) => !(v && v.length <= 8) || "Password Must be 8 Charechters or More",
+    ],
+    specialization: ["ITE", "DU"],
+
+    email: "",
+    emailRules: [
+      (v) => !!v || "E-mail is required",
+      (v) => /.+@.+\..+/.test(v) || "E-mail must be valid",
+    ],
+    checkbox: false, 
+    date: new Date().toISOString().substr(0, 10),
+    date2: new Date().toISOString().substr(0, 10),
+    menu: false,
+    menu2: false,
     
   }),
-}
+  methods: {
+    validate() {
+      this.$refs.form.validate();
+    },
+    reset() {
+      this.$refs.form.reset();
+    },
+  },
+};
 </script>
 
 <style>
+.Blured {
+  backdrop-filter: blur(3px);
+  background: rgba(255, 255, 255, 0.7);
+}
 .colortab {
-  color:#43425D !important ;
+  color: #43425d !important ;
 }
-.pdtop2{
-  padding-top: 20px;
+.midcard {
+  min-height: 400px;
 }
-.Pdtop {
-  padding-top: 65px;
-}
-
-
-.midcard{
-  height: 500px;
-  
-}
-.Flex{
+.Flex {
   flex-direction: column;
   padding-top: 35px;
 }
-
 .back {
-  background-image: url('~@/assets/LoginBackground.jpeg');
+  background-image: url("~@/assets/LoginBackground.jpeg");
   background-size: cover;
   width: 100%;
- 
+}
 
+.blured {
+  backdrop-filter: blur(10px);
+  background: rgba(255, 255, 255, 0.7);
 }
 </style>
