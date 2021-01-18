8422cc9d1dcf022adc2e26e16512a1064945ba5c
Firas Allam <allamfi89@gmail.com>
2021-01-18 18:32:48 +0200

    dark Mode

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,13 +4,17 @@ import Vuetify from 'vuetify/lib/framework';
 Vue.use(Vuetify);
 
 export default new Vuetify({
-    theme: {
-        themes: {
-          light: {
-            primary: '#3c3b54',
-            secondary: '#a3a0fb',
-            accent: '#a5a4bf',
-          },
-        },
+  theme: {
+    themes: {
+      light: {
+        primary: '#3c3b54',
+        secondary: '#a3a0fb',
+        accent: '#a5a4bf',
       },
+      dark: {
+        primary: 'colors.blue.lighten3',
+        background: 'colors.indigo.base',
+      },
+    },
+  },
 });
