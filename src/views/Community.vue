dbb38e3f5dd5f40a42738fb085abd6ee803f849c
Roduan Kareem Aldeen <roduan98@gmail.com>
2021-01-26 18:59:10 +0200

    add the create post modal on community page

--- src/views/Community.vue
+++ src/views/Community.vue
@@ -50,6 +50,83 @@
         </v-card>
       </v-col>
       <post-details-dialog :post="item"></post-details-dialog>
+      <v-dialog
+      v-model="addDialog"
+      persistent
+      max-width="600px"
+    >
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn
+        color="primary"
+        elevation="2"
+        fab
+        fixed
+        bottom
+        right
+        v-bind="attrs"
+        v-on="on"
+      >
+        <v-icon>mdi-plus</v-icon>
+      </v-btn>
+      </template>
+      <v-card>
+        <v-card-title>
+          <span class="headline">Add New Post</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container>
+            <v-row>
+              <v-col
+                cols="12"
+                sm="6"
+                md="5"
+              >
+                <v-text-field
+                  label="Post Title*"
+                  v-model="post.title"
+                  required
+                ></v-text-field>
+              </v-col>
+              <v-col
+                cols="12"
+                sm="6"
+                md="7"
+              >
+                <v-text-field
+                  v-model="post.attach"
+                  label="Post Image"
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12">
+                <v-textarea
+                  v-model="post.value"
+                  label="Post Content*"
+                  required
+                ></v-textarea>
+              </v-col>
+            </v-row>
+          </v-container>
+          <small>*indicates required field</small>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="primary darken-1"
+            text
+            @click="addDialog = false"
+          >
+            Close
+          </v-btn>
+          <v-btn
+            color="orange darken-1"
+            text
+            @click="savePost"
+          >
+            Save
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
     </v-row>
   </v-container>
 </template>
@@ -67,17 +144,35 @@ export default {
     items: [],
     item: {},
     dialog: false,
+    addDialog: false,
+    post: {}
   }),
   mounted() {
     const self = this
-    this.axios.get(`/posts/getallposts/${self.$store.getters.authenticated.id}`).then((res) => {
-      self.items = res.data
-    })
+    
+    self.fetchPosts()
   },
   methods: {
+    fetchPosts () {
+      const self = this
+      self.axios.get(`/posts/getallposts/${self.$store.getters.authenticated.id}`).then((res) => {
+        self.items = res.data
+      })
+    },
     openDialog (index) {
       this.item = this.items[index]
       this.$eventBus.$emit('TOGGLE_DIALOG')
+    },
+    savePost () {
+      const self = this
+
+      self.axios.post(`/posts/add/${self.$store.getters.authenticated.id}`, self.post).then(() => {
+        self.addDialog = false
+        self.fetchPosts()
+      })
+      .catch(() => {
+        alert('something went wrong')
+      })
     }
   }
 }
